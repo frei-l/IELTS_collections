af197c25bd2459610a81e800c38a8a6ea83fcc16
freil <l@freil.me>
2024-10-28 21:44:35 +0800

    astro

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://freil.github.io',
+    base: 'IELTS_collections',
+    markdown: {
+        shikiConfig: {
+            themes: {
+                light: 'snazzy-light',
+            },
+        },
+    },
+});
