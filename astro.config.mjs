120cffb3453a176781aaee2f6e51ed5aa84e13b8
freil <l@freil.me>
2024-10-29 00:14:16 +0800

    bugfix baseURL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://freil.github.io',
-    base: 'IELTS_collections',
+    base: '/IELTS_collections/',
     markdown: {
         shikiConfig: {
             themes: {
